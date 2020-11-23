3dd3c9e21afdc86ee2f984719a8053a2c9c4ee6a
vinremy <43629967+vinremy@users.noreply.github.com>
2020-11-23 17:44:27 -0500

    commit 2

---
+++ jeu_et_interface/interface/scripts/interface.js
@@ -0,0 +1,58 @@
+export class Interface {
+
+
+    constructor() {
+
+        this.socket = io("https://vremy.dectim.ca:3011", {
+            query: {type: "interface"}
+        });
+
+
+
+
+
+
+        // Bouton pour demander la permission d'accéder aux données de positionnement
+        document.querySelector("button").addEventListener("click", this.autoriser.bind(this));
+
+
+
+
+    }
+
+
+
+    autoriser(e) {
+
+        // Vérifier si les événements d'orientation sont disponibles sur cette plateforme
+        if (!window.DeviceOrientationEvent || !window.DeviceOrientationEvent.requestPermission) {
+            alert("Oups! Votre fureteur ne supporte pas la détection d'orientation.");
+            return;
+        }
+
+        // Demande de permission à l'usager pour l'utilisation des événements d'orientation
+        DeviceOrientationEvent.requestPermission()
+            .then(state => {
+                if (state === 'granted') {
+                    window.addEventListener('deviceorientation', this.gererOrientation.bind(this));
+                }
+            });
+
+    }
+
+
+
+
+
+
+
+    gererOrientation(e) {
+
+
+        this.socket.emit("mouvement", {type : "mouvement" ,alpha: e.alpha, beta: e.beta, gamma: e.gamma});
+
+    }
+
+
+
+}
